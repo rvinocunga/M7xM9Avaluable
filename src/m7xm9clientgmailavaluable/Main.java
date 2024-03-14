acc4b3989448289736ee27c378f3a6a560ba42b8
rvinocunga <adam22rvinocunga@inslaferreria.cat>
2024-03-14 17:40:27 +0100

    init

---
+++ src/m7xm9clientgmailavaluable/Main.java
@@ -0,0 +1,12 @@
+package m7xm9clientgmailavaluable;
+
+/**
+ *
+ * @author roger
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("prueba");
+    }
+    
+}
